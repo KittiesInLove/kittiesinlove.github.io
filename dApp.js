16036dbf721eb80e1dc31f9b6eb30d7b8309e254
Jeremias <l@lll.uno>
2017-12-06 14:16:16 +0100

    test

--- dApp.js
+++ dApp.js
@@ -69,6 +69,7 @@ var accountInterval = setInterval(function() {
       return $("#checkBalanceResponse_body").html(String("I own " + amount.toString(10)) + " Kitties:");
   });
   contract.tokensOfOwner(account, function(error, kitties){
+    console.log(kitties);
     return $("#checkMyKittiesResponse_body").html(String(kitties.toString(10)));
   });
 /*
