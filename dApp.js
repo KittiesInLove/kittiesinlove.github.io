ddaa47577e90bb426f85a40e60424415125d2a58
Jeremias <l@lll.uno>
2017-12-06 12:11:36 +0100

    mm hide

--- dApp.js
+++ dApp.js
@@ -16,7 +16,6 @@ var kittyTokenAddress = "0x06012c8cf97BEaD5deAe237070F9587f8E7A266d"
 // setup metamask provider
 if(typeof window.web3.currentProvider !== 'undefined') {
     web3.setProvider(window.web3.currentProvider);
-        $("#checkMetamask").show();
 }
 else {
     web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
@@ -82,6 +81,11 @@ var accountInterval = setInterval(function() {
   });*/
   // auto refresh address
   $("#myAddress").html(account);
+  if (typeof web3 !== 'undefined') {
+    $("#checkMetamask").show();
+  } else {
+    $("#checkMetamask").hide();
+  }
 
 }, 2000);
 
