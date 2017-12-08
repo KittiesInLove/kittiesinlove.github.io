f6733138f86cf04d2e694ac64e83c441df36edb5
KittiesInLove <34302414+KittiesInLove@users.noreply.github.com>
2017-12-08 19:21:19 +0100

    added tip button

--- dApp.js
+++ dApp.js
@@ -161,3 +161,28 @@ $("#_breed").click(function(event){
 });
 
 // - Breeding
+
+// Tip Button
+
+var tipButton = document.querySelector('.tip-button')
+tipButton.addEventListener('click', function() {
+  if (typeof web3 === 'undefined') {
+    return renderMessage('You need to install MetaMask to use this feature.  https://metamask.io')
+  }
+
+  var user_address = web3.eth.accounts[0]
+  web3.eth.sendTransaction({
+    to: "0x29324031F7b722d2eb8fc7a1023C7033AAC53Cc0",
+    from: user_address,
+    value: web3.toWei('1', 'ether'),
+  }, function (err, transactionHash) {
+    if (err) return renderMessage('Oh no!: ' + err.message)
+
+    // If you get a transactionHash, you can assume it was sent,
+    // or if you want to guarantee it was received, you can poll
+    // for that transaction to be mined first.
+    renderMessage('Thanks!')
+  })
+})
+
+// - Tip Button
