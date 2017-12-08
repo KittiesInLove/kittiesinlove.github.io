008b053d2ae68f1f256966b9bb9e2f9d9ae50637
KittiesInLove <34302414+KittiesInLove@users.noreply.github.com>
2017-12-08 23:40:48 +0100

    Update dApp.js

--- dApp.js
+++ dApp.js
@@ -165,10 +165,9 @@ $("#_breed").click(function(event){
 // Tip Button
 var MY_ADDRESS = '0x29324031F7b722d2eb8fc7a1023C7033AAC53Cc0'
 
-var tipAmount = parseFloat($("#tipAmount").val())
-
 var tipButton = document.querySelector('.tip-button')
 tipButton.addEventListener('click', function() {
+  var tipAmount = $("#tipAmount").val()
   if (typeof web3 === 'undefined') {
     return renderMessage('You need to install MetaMask to use this feature.  https://metamask.io')
   }
