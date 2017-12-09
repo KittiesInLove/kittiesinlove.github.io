969f1c4582c768c86dd42b3392cca145832ecd58
KittiesInLove <l@lll.uno>
2017-12-09 19:57:33 +0100

    nan check

--- dApp.js
+++ dApp.js
@@ -132,7 +132,7 @@ $("#_checkbreed").click(function(event){
 
   contract.canBreedWith(_sireID, _breedID, function(_hash, _valid) {
           console.log(_sireID);
-    if(_sireID < 1 || _breedID < 1 || _sireID == null || _breedID == null) {
+    if(_sireID < 1 || _breedID < 1 || _sireID == NaN || _breedID == NaN) {
         $("#approveBreedResponse").show();
         return $("#approveBreedResponse_body").html("Error: Invalid or empty Kitty-ID");
     }
