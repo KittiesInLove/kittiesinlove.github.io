cdd57788037be4ba1f02dda32864aae3b8a3b56f
KittiesInLove <l@lll.uno>
2017-12-09 16:41:22 +0100

    replaced by canBreedWith

--- dApp.js
+++ dApp.js
@@ -126,7 +126,7 @@ $("#_checkbreed").click(function(event){
 
   console.log("Breeding Details", _sireID, _breedID);
 
-  contract._isSiringPermitted(_sireID, _breedID, function(error, transactionHash) {
+  contract.canBreedWith(_sireID, _breedID, function(error, transactionHash) {
     if(_sireId < 1 || _breedID < 1) {
         $("#approveBreedResponse").show();
         return $("#approveBreedResponse_body").html("Error: Invalid or empty Kitty-ID");
